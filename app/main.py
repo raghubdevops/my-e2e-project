87b1080a139643046231f03253a49388426b1a1d
raghubdevops <raghubdevops@outlook.com>
2026-07-04 04:45:33 +0530

    test full automation

--- app/main.py
+++ app/main.py
@@ -2,9 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#@app.get("/")
+#def home():
+ #   return {"message": "Hello from my E2E project!"}
+
 @app.get("/")
 def home():
-    return {"message": "Hello from my E2E project!"}
+    return {"message": "Hello from my E2E project! Pipeline is fully automated!"}
 
 @app.get("/health")
 def health():
